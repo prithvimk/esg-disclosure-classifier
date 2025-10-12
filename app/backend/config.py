d342859ffac6f49a31c3f88bf86b1299b08128ef
Prithvi Krishna <krishna.prithvi2001@gmail.com>
2025-10-12 17:17:15 +0530

    chore: deleted old files

--- app/backend/config.py
+++
@@ -1,4 +0,0 @@
-MILVUS_HOST = "localhost"   # or "milvus" if using docker-compose
-MILVUS_PORT = "19530"
-COLLECTION_NAME = "esg_chunks"
-EMBED_DIM = 384   # match embedding model output (e.g., MiniLM = 384)
