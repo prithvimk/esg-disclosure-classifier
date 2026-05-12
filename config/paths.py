dc3441f54ce4f5cb363bdaad36b99851673535f1
Prithvi Krishna <krishna.prithvi2001@gmail.com>
2026-05-12 23:49:58 +0530

    starting from scratch

--- config/paths.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-DISC_STANDARDS = BASE_DIR / "disclosure_standards"
-DISC_STANDARDS_RAW = DISC_STANDARDS / "raw"
-DISC_STANDARDS_PROCESSED = DISC_STANDARDS / "processed"
